a7e1a38baf267a9531761d95592b64bbec05ccc4
Tetz <tetz2281337@gmail.com>
2020-12-22 14:22:44 +0700

    Привязка свойства Статус к тексту в StatusBar

--- MVVMWPF/ViewModels/MainWindowViewModel.cs
+++ MVVMWPF/ViewModels/MainWindowViewModel.cs
@@ -16,5 +16,15 @@ namespace MVVMWPF.ViewModels
         }
 
         #endregion
+
+        #region Status : string - Статус программы
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готово!";
+
+        /// <summary>Статус программы</summary>
+        public string Status { get => _Status; set => Set(ref _Status, value); }
+
+        #endregion
     }
 }
