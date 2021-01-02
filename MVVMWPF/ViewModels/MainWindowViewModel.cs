0906e397b85e5abc0c2e5208f3d6771736ef0128
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-02 13:10:51 +0700

    Добавление тестовых данных в модель-представления

--- MVVMWPF/ViewModels/MainWindowViewModel.cs
+++ MVVMWPF/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using MVVMWPF.Infrastructure.Commands;
+using MVVMWPF.Models;
 using MVVMWPF.ViewModels.Base;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,6 +10,20 @@ namespace MVVMWPF.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
+        #region TestDataPoint : IEnumerable<DataPoint> - DESCRIPTION
+
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+
+        #endregion
+
         #region Заголовок окна
 
         private string _Title = "Анализ статистики MVVM";
@@ -40,7 +57,7 @@ namespace MVVMWPF.ViewModels
             Application.Current.Shutdown();
         }
 
-        private bool CanCloseApplicationCommandExecuted(object p) => true; 
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
         #endregion
 
         #endregion
@@ -50,6 +67,19 @@ namespace MVVMWPF.ViewModels
             #region Команды
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+
+            for (var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+
+                var y = Math.Sin(2 * Math.PI * x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            TestDataPoints = data_points;
         }
     }
 }
